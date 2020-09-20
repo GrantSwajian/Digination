16e027dbf16619c5194d20159f7bdc45b9441e8d
GrantSwajian <ggswajian5@gmail.com>
2020-09-19 21:16:05 -0700

    Delete connect.inc.php

--- connect.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-mysql_connect("http://grantswajian.freevar.com/","130242","Torpedo20073793") or die("Couldn't connect to SQL server");
-mysql_select_db("event") or die("couldn't select DB");
-?>
